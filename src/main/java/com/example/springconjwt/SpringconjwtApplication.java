7503ba4e5a25ccb13978f6e49a47f6f8cf527c50
mohid2 <fukahamaroc@gmail.com>
2023-08-15 12:42:32 +0200

    creación del proyecto con las dependencias necesarias

---
+++ src/main/java/com/example/springconjwt/SpringconjwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.springconjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringconjwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringconjwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springconjwt/SpringconjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springconjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringconjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
