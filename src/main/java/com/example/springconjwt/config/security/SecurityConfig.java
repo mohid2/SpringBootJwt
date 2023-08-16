5c0d2bf0a44f92c63e31419e8a97c89377bfbc8a
mohid2 <fukahamaroc@gmail.com>
2023-08-16 12:33:10 +0200

    creación de la clase de configuración SecurityConfig

---
+++ src/main/java/com/example/springconjwt/config/security/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.example.springconjwt.config.security;
+
+import com.example.springconjwt.config.security.Jwt.JwtAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(autRequest->
+                        autRequest.requestMatchers(publicEndpoints()).permitAll()
+                                .anyRequest().authenticated())
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+    private RequestMatcher publicEndpoints(){
+        return new OrRequestMatcher(
+                new AntPathRequestMatcher("/api/hello"),
+                new AntPathRequestMatcher("/api/auth/**")
+        );
+    }
+
+
+    }
+
