0d76c04a6503fc38d668cc3ad4a7cc53f653e50a
mohid2 <fukahamaroc@gmail.com>
2023-08-16 12:48:38 +0200

    creación de del controlador para login y register y sus modelos

---
+++ src/main/java/com/example/springconjwt/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.example.springconjwt.controller;
+
+import com.example.springconjwt.controller.models.AuthResponse;
+import com.example.springconjwt.controller.models.LoginRequest;
+import com.example.springconjwt.controller.models.RegisterRequest;
+import com.example.springconjwt.service.IAuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final IAuthService authService;
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request){
+        return ResponseEntity.ok(authService.register(request));
+    }
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request){
+        return ResponseEntity.ok(authService.login(request)) ;
+    }
+
+
+}
---
+++ src/main/java/com/example/springconjwt/controller/models/AuthResponse.java
@@ -0,0 +1,16 @@
+package com.example.springconjwt.controller.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthResponse {
+    private String message;
+    private String token;
+
+}
